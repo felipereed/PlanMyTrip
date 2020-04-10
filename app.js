c714255ff03cf4739975fc406c18104a3c5b7370
Felipe Reed <Felipe@Users-Air.fios-router.home>
2020-04-09 23:17:23 -0400

    finished project with comments

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-//From OMDI exercise 
+//this is the API key formatted for the authorization header
 const API_KEY = 'Bearer 4xXf6b67EXTMd9Mw4WkHO4-ivhSa7wSi252Lsi0Zvgs5FmNaSQVE2oTPH_1_tji5mVhZ4qhN_aq-jjGsK6Nrcgwic7UxQmaZNaEG43X_n5L054fE5WlbUdAMqgKJXnYx';
-//Add CORS authorization code Corey sent
+//Add CORS authorization code Corey sent plus the base API url
 const BASE_URL = 'https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/search?location=new-york&limit=50';
 
-let storedRecommendations = [];
-
+//this function calls the API passing the searchTerm and price, if there's one
 async function getInfoFromApi(searchTerm, price) {
   let urlWithParameter = BASE_URL + `&term=${searchTerm}`;
 
@@ -20,14 +19,17 @@ async function getInfoFromApi(searchTerm, price) {
   });
 
   console.log(response);
+  //returns the response from the API in the format
   return response.data.businesses;
 }
 //https://css-tricks.com/snippets/javascript/select-random-item-array/
+//this function randomly selects one object from the array and returns it
 function randonmlySelectItem(myArray) {
   return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
 //https://mkyong.com/javascript/javascript-get-selected-value-from-dropdown-list/
+//this function contains all the buttons and dropdowns and adds the event listeners
 function buttonEvents() {
   
   let breakfastDropdown = document.querySelector('.breakfast > select');
@@ -64,13 +66,14 @@ function buttonEvents() {
   closeRooftopRecommendation.addEventListener('click', hideResultSection);
 }
 // trying to make async because it did not work without it
+//this function calls the API, gets a random item from the results and call the populateResultSection function below to display info on the page
 async function makeRecommendation(searchTerm, priceRange) {
   let apiResults = await getInfoFromApi(searchTerm, priceRange);
   let randomItem = randonmlySelectItem(apiResults);
   console.log(randomItem);
   populateResultSection(randomItem, searchTerm);
 }
-
+//Event handlers that will call the function makeRecommendation() above
 async function selectBreakfast() {
   let priceRange = this.options[this.selectedIndex].value;
   makeRecommendation('Breakfast', priceRange);
@@ -103,24 +106,24 @@ async function buttonRooftop() {
 }
 
 buttonEvents();
-
+//this function populates the result section added under each category on the page. It also sets the display to block so it shows up. 
 function populateResultSection(item, category) {
   let img = document.getElementById('resultImg' + category);
   let name = document.getElementById('resultName' + category);
   let address = document.getElementById('resultAddress' + category);
   let phone = document.getElementById('resultPhone' + category);
   let website = document.getElementById('resultWebsite' + category);
-
+// the section below sets all the properties of the elements in the result section 
   img.src = item.image_url;
   name.innerHTML = item.name;
   address.innerHTML = item.location.display_address[0] + '</br>' + item.location.display_address[1];
   phone.innerHTML = item.display_phone;
   website.href = item.url;
-
+// so far the recommendation is invisible, this code displays it after the elements are populated
   img.parentElement.style.display = 'block';
 }
 
-//toggle hide and show solution https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+// toggle hide and show solution https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
 function hideResultSection() {
   this.parentElement.style.display = 'none';
   
